c8d6b42a643065a9d8fd641dfa6d511e265617eb
kutluhan <kutluhanyegit399@gmail.com>
2021-05-15 12:40:31 +0300

    UI Update

---
+++ Sky.UI/Controllers/girisController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sky.UI.Controllers
+{
+    public class girisController:Controller
+    {
+        public IActionResult index()
+        {
+          //TODO: Implement Realistic Implementation
+          return View();
+        }
+    }
+}
---
+++ Sky.UI/Controllers/model_yonetimiController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sky.UI.Controllers
+{
+    [Route("model-yonetimi")]
+    public class model_yonetimiController:Controller
+    {
+        public IActionResult index()
+        {
+          //TODO: Implement Realistic Implementation
+          return View();
+        }
+    }
+}
